9b3d98a21b64fa0ba79dbdc99f94f435ddfd81c0
Tholfikar <fukar.work@gmail.com>
2024-08-14 08:44:52 +0300

    Complete #1 challange

---
+++ #1 3x3 Random Matrix/3x3 Random Matrix/3x3 Random Matrix.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+int RandomNumber(int from = 1, int to = 100)
+{
+    int rand_num = rand() % (to - from + 1) + from;
+
+    return rand_num;
+}
+
+static void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            arr[i][j] = RandomNumber();
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            cout << setw(3) << arr[i][j] << "    ";
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    // Seed the random number generator in c++, call it only once.
+    srand((unsigned)time(NULL));
+
+    int arr[3][3];
+
+    FillMatrixWithRandomNumbers(arr, 3, 3);
+
+    cout << "\n3x3 Random Matrix\n";
+    PrintMatrix(arr, 3, 3);
+}
