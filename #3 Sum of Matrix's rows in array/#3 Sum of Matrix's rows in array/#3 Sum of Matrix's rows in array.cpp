de6be21538564307c322db4224e83d79b3a6ff35
Tholfikar <fukar.work@gmail.com>
2024-08-15 12:42:20 +0300

    complete #3 challange

---
+++ #3 Sum of Matrix's rows in array/#3 Sum of Matrix's rows in array/#3 Sum of Matrix's rows in array.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+// Forward declaration
+void PrintArrayOfSums(int arr[3], short Rows);
+
+int RandomNumber(int from = 1, int to = 100)
+{
+    int rand_num = rand() % (to - from + 1) + from;
+
+    return rand_num;
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            arr[i][j] = RandomNumber();
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            cout << setw(3) << arr[i][j] << "    ";
+        }
+        cout << "\n";
+    }
+}
+
+void PrintSumOfEachRowOfMatrix(int arr[3][3], short Rows, short Cols)
+{
+    int SumsArr[3] = {};
+    int RowSum = 0;
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            RowSum += arr[i][j];
+        }
+
+        cout << "\nRow " << (i + 1) << " Sum = " << RowSum << endl;
+        SumsArr[i] = RowSum;
+        RowSum = 0;
+    }
+
+    PrintArrayOfSums(SumsArr, Rows);
+}
+
+void PrintArrayOfSums(int arr[3],short Rows)
+{
+    cout << "\nThe array of Sums is: \n";
+    for (int i = 0; i < Rows; i++) {
+        cout << arr[i] << "   ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    // Seed the random number generator in c++, call it only once.
+    srand((unsigned)time(NULL));
+
+    int arr[3][3];
+
+    FillMatrixWithRandomNumbers(arr, 3, 3);
+
+    cout << "The following is a 3x3 Random Matrix\n";
+    PrintMatrix(arr, 3, 3);
+
+    cout << "\nThe following are the sum of each row in Matrix\n";
+    PrintSumOfEachRowOfMatrix(arr, 3, 3);
+
+}
