06b161de8844ab74b2a8a11c17feb8ecd0e83c27
Tholfikar <fukar.work@gmail.com>
2024-09-05 03:15:20 +0300

    complete #23 challange

---
+++ #23 Print First Letter of Each Word/#23 Print First Letter of Each Word/#23 Print First Letter of Each Word.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string ReadString()
+{
+	string S1;
+	cout << "Please Enter Your String\n";
+	getline(cin, S1);
+	return S1;
+}
+
+void PrintFirstLetterOfEachWord(string S1)
+{
+	bool isFirstLetter = true;
+
+	cout << "\nFirst letters of this string:\n";
+
+	for (int i = 0; i < S1.length(); i++)
+	{
+		if (S1[i] != ' ' && isFirstLetter)
+		{
+			cout << S1[i] << endl;
+		}
+
+		isFirstLetter = (S1[i] == ' ' ? true : false);
+	}
+}
+
+int main()
+{
+    string S = "Hello World, this is C++ Course";
+	//PrintFirstLetterOfEachWord(ReadString());
+	PrintFirstLetterOfEachWord(S);
+}
