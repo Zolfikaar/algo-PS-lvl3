2dcb79b6f4f15558587621a787fd8405528ba117
Tholfikar <fukar.work@gmail.com>
2024-09-26 05:47:22 +0300

    complete #39 challange

---
+++ #39 Join String/#39 Join String/#39 Join String.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string JoinString(vector <string> vString, string Delim)
+{
+    string S1;
+
+    for (string& s : vString)
+        S1 = S1 + s + Delim;
+
+    return S1.substr(0, S1.length() - Delim.length());
+}
+
+int main()
+{
+    vector <string> vString{"Mohammed", "Ahmed", "Hassan", "Ali"};
+    cout << "Vector after join \n";
+    cout << JoinString(vString, " ");
+    system("pause>0");
+}
